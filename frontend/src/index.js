df9c3789e49d31bdf4f7a1232d1ca2eb84027e37
ManuBlue <manugadde@gmail.com>
2024-06-06 02:41:10 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <RouterProvider>
+    <BrowserRouter>
     <nav />
       <App />
-    </RouterProvider>
+    </BrowserRouter>
 
 );
 
