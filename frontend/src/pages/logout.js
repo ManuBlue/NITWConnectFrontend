e92d8c36ed349295d07cf37e1f2b2ff79e5d0fd0
ManuBlue <manugadde@gmail.com>
2024-06-06 08:26:45 +0530

    Update logout.js

--- frontend/src/pages/logout.js
+++ frontend/src/pages/logout.js
@@ -1,38 +1,19 @@
 import React, { useState, useEffect } from "react";
-//import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+
 function Logout() {
   const navigate = useNavigate();
-  const [message, setMessage] = useState("Failed to logout : c");
-  // const navigate = useNavigate();
+  const [message, setMessage] = useState("Failed to logout :(");
 
-  // useEffect(() => {
-  //   fetch('/mydata', { credentials: 'include' })
-  //     .then(response => {
-  //       if (response.status === 401) {
-  //         return null;
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(mydata => {
-  //       if (mydata && mydata.username) {
-  //           axios.post('http://localhost:5000/logout', 1, { withCredentials: true })
-  //           .then(() => {
-  //               setMessage("Successfully logged out!");
-  //             })
-  //       }
-  //       setTimeout(() => {
-  //           navigate("/");
-  //         }, 1000); 
-  //     });
-  // }, [navigate]);
-  localStorage.clear()
-  .then(() => {
+  useEffect(() => {
+    localStorage.clear();
     setMessage("Successfully logged out!");
-  })
-  setTimeout(() => {
-    navigate("/");
-  }, 1000); 
+
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  }, [navigate]);
+
   return (
     <>
       <h1>{message}</h1>
