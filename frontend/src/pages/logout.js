f3427070e796fdddf04f0e824f9d9339483c0208
ManuBlue <manugadde@gmail.com>
2024-06-06 08:20:51 +0530

    Update logout.js

--- frontend/src/pages/logout.js
+++ frontend/src/pages/logout.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 function Logout() {
   const navigate = useNavigate();
-  // const [message, setMessage] = useState("Failed to logout : c");
+  const [message, setMessage] = useState("Failed to logout : c");
   // const navigate = useNavigate();
 
   // useEffect(() => {
